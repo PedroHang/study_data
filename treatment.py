bcdbcbacca771683124cebf13affef758fae0b9c
Pedro Lucas Azevedo Hang <hangpedroo@hotmail.com>
2024-10-05 23:50:28 -0300

    aaa

--- treatment.py
+++ treatment.py
@@ -36,7 +36,6 @@ if not df.empty:
     # Format the 'Full_Date' column to datetime
     df['Full_Date'] = pd.to_datetime(df['Full_Date'])
 
-    # Continue with your existing charts and analysis...
     # Group by 'Full_Date' and sum the hours
     df_daily = df.groupby("Full_Date")['Hours'].sum().reset_index()
 
@@ -44,11 +43,8 @@ if not df.empty:
     df_daily['Rolling Volatility'] = df_daily['Hours'].rolling(window=7).std()
 
     # Calculate the weekly average starting from Monday
-    df_daily['Week'] = df_daily['Full_Date'].dt.to_period('W').apply(lambda r: r.start_time)  # Get the start date of the week
+    df_daily['Week'] = df_daily['Full_Date'].dt.to_period('W').apply(lambda r: r.start_time)
     df_weekly = df_daily.groupby('Week')['Hours'].mean().reset_index()
-
-    # Format the average hours to 2 decimal points
-    df_weekly['Hours'] = df_weekly['Hours'].round(2)
 
     # --- Longest Study Streak Calculation ---
 
@@ -67,7 +63,25 @@ if not df.empty:
     df_streaks = df_daily[df_daily['Studied']].groupby('Streak_Group').size().reset_index(name='Streak_Length')
     max_streak = df_streaks['Streak_Length'].max()
 
-    # Check if df_daily is not empty before plotting
+    # Display metrics
+    col1, col2, col3, col4 = st.columns(4)
+
+    # Calculate the total hours studied
+    total_hours = df['Hours'].sum()
+    days_equivalent = total_hours / 24
+
+    # Calculate the maximum hours for a single day
+    max_hours_day = df_daily.loc[df_daily['Hours'].idxmax()]
+
+    # Display the total hours and streak in col1
+    col1.metric(label="Total Hours Studied", value=f"{total_hours:.2f} Hours")
+    col1.metric(label="Total Study Time in Days", value=f"{days_equivalent:.2f} Days")
+    col1.metric(label="Longest Study Streak", value=f"{max_streak} Days")
+
+    # Display the max hours in a single day in col2
+    col2.metric(label="Max Hours in a Single Day", value=f"{max_hours_day['Hours']:.2f} Hours", delta=f"{max_hours_day['Full_Date'].strftime('%Y-%m-%d')}")
+
+    # --- Existing chart functionality ---
     if not df_daily.empty:
         # Toggle between Total Hours, Rolling Volatility, and Weekly Average
         plot_option = st.selectbox("Select Plot Type:", ("Total Hours", "Rolling Volatility", "Weekly Average"))
@@ -80,15 +94,14 @@ if not df.empty:
                           markers=True)
 
             # Update layout for wider dimensions and improve aesthetics
-            fig.update_traces(line=dict(width=4, color='orange'),  # Set line width and color
-                              marker=dict(size=8, symbol='circle'))  # Set marker size and shape
-            fig.update_layout(title_font=dict(size=24),  # Title font size
-                              xaxis_title_font=dict(size=18),  # X-axis title font size
-                              yaxis_title_font=dict(size=18),  # Y-axis title font size
-                              legend=dict(title_font=dict(size=16), font=dict(size=14)),  # Legend font size
-                              width=1800, height=460)  # Update dimensions
+            fig.update_traces(line=dict(width=4, color='orange'),
+                              marker=dict(size=8, symbol='circle'))
+            fig.update_layout(title_font=dict(size=24),
+                              xaxis_title_font=dict(size=18),
+                              yaxis_title_font=dict(size=18),
+                              legend=dict(title_font=dict(size=16), font=dict(size=14)),
+                              width=1800, height=460)
 
-            # Show the Plotly chart in Streamlit
             st.plotly_chart(fig)
 
         elif plot_option == "Rolling Volatility":
@@ -98,16 +111,14 @@ if not df.empty:
                                      labels={'Full_Date': 'Date', 'Rolling Volatility': 'Volatility'},
                                      markers=True)
 
-            # Update layout for wider dimensions and improve aesthetics
-            fig_volatility.update_traces(line=dict(width=4, color='royalblue'),  # Set line width and color
-                                         marker=dict(size=8, symbol='circle'))  # Set marker size and shape
-            fig_volatility.update_layout(title_font=dict(size=24),  # Title font size
-                                          xaxis_title_font=dict(size=18),  # X-axis title font size
-                                          yaxis_title_font=dict(size=18),  # Y-axis title font size
-                                          legend=dict(title_font=dict(size=16), font=dict(size=14)),  # Legend font size
-                                          width=1800, height=600)  # Update dimensions
+            fig_volatility.update_traces(line=dict(width=4, color='royalblue'),
+                                         marker=dict(size=8, symbol='circle'))
+            fig_volatility.update_layout(title_font=dict(size=24),
+                                          xaxis_title_font=dict(size=18),
+                                          yaxis_title_font=dict(size=18),
+                                          legend=dict(title_font=dict(size=16), font=dict(size=14)),
+                                          width=1800, height=600)
 
-            # Show the Plotly chart in Streamlit
             st.plotly_chart(fig_volatility)
 
         elif plot_option == "Weekly Average":
@@ -115,81 +126,17 @@ if not df.empty:
             fig_weekly = px.bar(df_weekly, x='Week', y='Hours',
                                  title='Weekly Average Study Hours',
                                  labels={'Week': 'Week Start Date', 'Hours': 'Average Hours'},
-                                 text=df_weekly['Hours'].apply(lambda x: f"{x:.2f}"),  # Format text to 2 decimal points
-                                 color='Hours',  # Color by average hours
-                                 color_continuous_scale=px.colors.sequential.Viridis)  # Color scale
+                                 text=df_weekly['Hours'].apply(lambda x: f"{x:.2f}"),
+                                 color='Hours',
+                                 color_continuous_scale=px.colors.sequential.Viridis)
 
-            # Update layout for wider dimensions and improve aesthetics
-            fig_weekly.update_traces(marker=dict(line=dict(width=1, color='black')))  # Add outline to bars
-            fig_weekly.update_layout(title_font=dict(size=24),  # Title font size
-                                      xaxis_title_font=dict(size=18),  # X-axis title font size
-                                      yaxis_title_font=dict(size=18),  # Y-axis title font size
-                                      legend=dict(title_font=dict(size=16), font=dict(size=14)),  # Legend font size
-                                      width=1800, height=600)  # Update dimensions
+            fig_weekly.update_traces(marker=dict(line=dict(width=1, color='black')))
+            fig_weekly.update_layout(title_font=dict(size=24),
+                                      xaxis_title_font=dict(size=18),
+                                      yaxis_title_font=dict(size=18),
+                                      legend=dict(title_font=dict(size=16), font=dict(size=14)),
+                                      width=1800, height=600)
 
-            # Show the Plotly chart in Streamlit
             st.plotly_chart(fig_weekly)
-
-    else:
-        st.warning("No data available for daily hours.")
-
-
-    # --- New Section for Visualization by Study ---
-
-    st.subheader("Total Hours by Subject")
-
-    # Group by 'Study' and sum the hours
-    df_study = df.groupby("Study")['Hours'].sum().reset_index()
-
-    # Sort the DataFrame by Hours in descending order
-    df_study = df_study.sort_values(by='Hours', ascending=False)
-
-    # Check if df_study is not empty before plotting
-    if not df_study.empty:
-        # Create a Plotly bar chart for total hours by study with a color palette
-        fig_study = px.bar(df_study, x='Study', y='Hours', 
-                           title='Total Hours by Study',
-                           labels={'Study': 'Study', 'Hours': 'Total Hours'},
-                           text='Hours',  # Display hours on the bars
-                           color='Hours',  # Color by hours
-                           color_continuous_scale=px.colors.sequential.Viridis)  # Beautiful color palette
-        
-        # Update layout for wider dimensions
-        fig_study.update_layout(width=1800)
-
-        # Show the Plotly chart in Streamlit
-        st.plotly_chart(fig_study)
-    else:
-        st.warning("No data available for study breakdown.")
-
-    # Get the last 15 days
-    last_15_days = datetime.now() - timedelta(days=15)
-    df_recent = df[df['Full_Date'] >= last_15_days]
-
-    # Group by 'Study' and sum the hours
-    df_study_recent = df_recent.groupby("Study")['Hours'].sum().reset_index()
-
-    # Sort the DataFrame by Hours in descending order and get the top 3 subjects
-    top_studies = df_study_recent.sort_values(by='Hours', ascending=False).head(3)
-
-    # Display metrics for the top 3 subjects
-    st.subheader("Top 3 Subjects Studied in the Last 15 Days")
-
-    for index, row in top_studies.iterrows():
-        st.metric(label=row['Study'], value=f"{row['Hours']:.2f} Hours")
-
-    # --- Display the total hours studied ---
-    # Calculate the total hours studied
-    total_hours = df['Hours'].sum()
-    days_equivalent = total_hours / 24
-
-    # Calculate the maximum hours for a single day
-    max_hours_day = df_daily.loc[df_daily['Hours'].idxmax()]
-
-    # Display metrics for the longest study streak, total hours, and max hours on a day
-    st.metric(label="Longest Study Streak", value=f"{max_streak} Days")
-    st.metric(label="Total Hours Studied", value=f"{total_hours:.2f} Hours")
-    st.metric(label="Total Study Time in Days", value=f"{days_equivalent:.2f} Days")
-    st.metric(label="Max Hours in a Single Day", value=f"{max_hours_day['Hours']:.2f} Hours", delta=f"{max_hours_day['Full_Date'].strftime('%Y-%m-%d')}")
 else:
     st.warning("No data available.")
